30fa1d3dd4ad94f8e4edee140ca68d42d6164491
Alex <alextielens48@gmail.com>
2026-03-30 22:30:57 -0400

    Update site content

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ const COMPANY = {
 
 // List your photo files here (put them in /images/work/)
 const WORK_PHOTOS = [
-  // "job-1.jpg",
-  // "job-2.jpg",
-  // "job-3.jpg",
+  "IMG_1.jpg",
+  "IMG_2.jpg",
+  "IMG_3.jpg",
+  "IMG_4.jpg",
+  "IMG_5.jpg",
+  "IMG_6.jpg",
+  "IMG_7.jpg",
+  "IMG_8.jpg",
+  "IMG_9.jpg",
+  "IMG_10.jpg",
+  "IMG_11.jpg",
+  "IMG_12.jpg",
+  "IMG_13.jpg",
+  "IMG_14.jpg",
+  "IMG_15.jpg",
+  "IMG_16.jpg",
+  "IMG_17.jpg",
 ];
 
 // ==== INIT ====
